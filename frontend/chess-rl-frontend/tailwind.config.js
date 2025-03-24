86d526db4f46aedcd3b937e9ae918c4e58b36111
Maks Szyło <maksym.szylo@gmail.com>
2025-03-24 23:31:28 +0100

    feat: added tailwind

---
+++ frontend/chess-rl-frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      margin: {
+        '5p': '5%',
+        '10p': '10%',
+        '15p': '15%',
+        '20p': '20%',
+        '25p': '25%',
+        '30p': '30%',
+        '35p': '35%',
+        '40p': '40%',
+        '45p': '45%',
+        '50p': '50%',
+      }
+    },
+  },
+  plugins: [],
+}
