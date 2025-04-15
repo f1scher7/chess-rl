a55e73486b895ccdb463ec3449b35a7f6e2f1851
Maks Szyło <maksym.szylo@gmail.com>
2025-04-15 17:24:13 +0200

    feat(utils.py): added common utils

---
+++ backend/utils/utils.py
@@ -0,0 +1,33 @@
+import torch
+from datetime import datetime
+from backend.config import SAVED_MODELS_PATH
+
+
+class Utils:
+
+    @staticmethod
+    def save_model(model, optimizer):
+        filename = f"chess-rl-by-f1scher7-{datetime.now().strftime('%H-%M-%S_%d-%m-%Y')}.pth"
+        final_path = SAVED_MODELS_PATH + filename
+
+        torch.save({
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }, final_path)
+
+        print(f"Model was saved: {final_path}")
+
+
+    @staticmethod
+    def load_model(model, optimizer, path):
+        checkpoint = torch.load(path)
+
+        if model and 'model_state_dict' in checkpoint:
+            model.load_state_dict(checkpoint['model_state_dict'])
+
+        if optimizer and 'optimizer_state_dict' in checkpoint:
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        print(f"Loaded!")
+
+
