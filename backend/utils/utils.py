6c1858db92f4d07464ec16864c8599b080deae97
Maks Szyło <maksym.szylo@gmail.com>
2025-05-02 17:27:02 +0200

    feat(utils.py): added get_device func

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -6,6 +6,11 @@ from backend.config import SAVED_MODELS_PATH
 class Utils:
 
     @staticmethod
+    def get_device():
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    @staticmethod
     def save_model(model, optimizer):
         filename = f"chess-rl-by-f1scher7-{datetime.now().strftime('%H-%M-%S_%d-%m-%Y')}.pth"
         final_path = SAVED_MODELS_PATH + filename
