fb0f2c573f1aa8a5a1930e912ba6a03e22cb7f12
Maks Szyło <maksym.szylo@gmail.com>
2025-05-07 23:13:02 +0200

    feat(Home): added logic for FileListDialog in Home component

--- frontend/chess-rl-frontend/src/pages/home/Home.tsx
+++ frontend/chess-rl-frontend/src/pages/home/Home.tsx
@@ -1,20 +1,45 @@
-import React from "react"
-import "./Home.css"
+import React, { useState } from "react";
+import "./Home.css";
+import FileListDialog from "../../components/fileListDialog/FileListDialog";
 import Footer from "../../components/Footer";
+import { fetchFileList } from "../../api/fetcher";
+
 
 const Home: React.FC = () => {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const [playGameVsAgent, setPlayGameVsAgent] = useState<boolean>(false);
+    const [fileList, setFileList] = useState<string[]>([]);
+
+    const handleOpenModal = async (playGame: boolean) => {
+        setPlayGameVsAgent(playGame);
+        setShowModal(true);
+
+        const selectedPathType = playGame ? "models" : "games";
+
+        try {
+            const data = await fetchFileList(selectedPathType);
+            setFileList(data.file_names);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <div className="container-fluid home-bg w-100">
             <div className="row h-75">
                 <div className="col">
-                    <div className="col d-flex flex-column bg-light rounded-3 shadow bg-opacity-75 w-25 h-100 ml-30p mt-5p">
+                    <div className="d-flex flex-column bg-light rounded-3 shadow bg-opacity-75 w-25 h-100 ml-30p mt-5p">
                         <div className="pt-15p text-center">
                             <h1>ChessRl</h1>
                             <img className="mx-auto d-block" src="/assets/chess-rl-logos/chess-rl-logo128.png" alt="ChessRl" />
                         </div>
-                        <div className="col d-flex flex-column align-items-center pt-20p gap-3">
-                            <button className="btn btn-dark w-50 py-2">See How AI Agents Play</button>
-                            <button className="btn btn-dark w-50 py-2">Play a Game Against AI Agent</button>
+                        <div className="d-flex flex-column align-items-center pt-20p gap-3">
+                            <button className="btn btn-dark w-50 py-2" onClick={() => handleOpenModal(false)}>See How AI Agents Play</button>
+                            <button className="btn btn-dark w-50 py-2" onClick={() => handleOpenModal(true)}>Play a Game Against AI Agent</button>
                             <a className="w-50" href="https://github.com/f1scher7/chess-rl" target="_blank" rel="noreferrer">
                                 <button className="btn btn-dark w-100 py-2">About</button>
                             </a>
@@ -25,9 +50,14 @@ const Home: React.FC = () => {
                     </div>
                 </div>
             </div>
+
+            <FileListDialog show={showModal}
+                            playGameVsAgent={playGameVsAgent}
+                            fileList={fileList}
+                            handleClose={handleCloseModal}
+            />
         </div>
     );
 };
-
 
 export default Home;
