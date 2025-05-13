a91427ec2bbfc0f705f52482da6098176cbefc2f
Maks Szyło <maksym.szylo@gmail.com>
2025-05-13 22:13:51 +0200

    feat(App): added routes for /agents-play and /user-play

--- frontend/chess-rl-frontend/src/App.tsx
+++ frontend/chess-rl-frontend/src/App.tsx
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ChessBoardPage from "./pages/chessBoardPage/ChessBoardPage";
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/agents-play" element={<ChessBoardPage isView={true} />} />
+                <Route path="/user-play" element={<ChessBoardPage isView={false} />} />
+            </Routes>
+        </Router>
+    );
 }
-
 
 export default App;
