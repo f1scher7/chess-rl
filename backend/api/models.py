336e9e37d9c1ad3eafc01954cbdf249c45031ae0
Maks Szyło <maksym.szylo@gmail.com>
2025-05-19 19:18:40 +0200

    feat(api): added Move and ModelStore

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from pydantic import BaseModel
 from typing import List
+from cachetools import LRUCache
+from backend.utils.utils import Utils
 
 
 class ChessRlFile(BaseModel):
@@ -24,4 +26,17 @@ class SavedGameContent(BaseModel):
 
 
 class Move(BaseModel):
-    move: str
+    move_str: str
+
+
+class ModelStore:
+    _cache = LRUCache(maxsize=7)
+
+    @classmethod
+    def load_model(cls, model_name):
+        if model_name not in cls._cache:
+            model, optimizer = Utils.create_default_model_and_optimizer()
+            Utils.load_model(model=model, optimizer=optimizer, file_name=model_name)
+            cls._cache[model_name] = (model, optimizer)
+
+        return cls._cache[model_name]
