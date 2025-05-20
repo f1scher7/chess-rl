5100761ff51a0c977eb43037c0cf14aa3c353617
Maks Szyło <maksym.szylo@gmail.com>
2025-05-20 10:44:03 +0200

    feat(utils): added create_default_model_and_optimizer func and added path to load_model func, refactoring

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,11 +1,13 @@
+import os
 import torch
+import torch.optim as optim
 import chess.pgn
 import matplotlib.pyplot as plt
 from datetime import datetime
 from io import StringIO
-from backend.chess_agent.agent_config import UPDATE_FREQUENCY, EPISODES
+from backend.chess_agent.agent_config import UPDATE_FREQUENCY, EPISODES, LEARNING_RATE
+from backend.chess_agent.policy import ChessPolicy
 from backend.config import SAVED_MODELS_PATH
-from backend.api.models import SavedGameContent
 
 
 class Utils:
@@ -30,7 +32,9 @@ class Utils:
 
 
     @staticmethod
-    def load_model(model, optimizer, path):
+    def load_model(model, optimizer, file_name):
+        path = os.path.join(SAVED_MODELS_PATH, file_name)
+
         checkpoint = torch.load(path)
 
         if model and 'model_state_dict' in checkpoint:
@@ -40,6 +44,25 @@ class Utils:
             optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
         print(f"Model and optimizer were loaded!")
+
+
+    @staticmethod
+    def create_default_model_and_optimizer():
+        device = Utils.get_device()
+
+        default_model = ChessPolicy(
+            conv_layers_num=3,
+            in_channels_list=[12, 64, 128],
+            out_channels_list=[64, 128, 256],
+            kernel_size_list=[3, 3, 3],
+            fc_layers_num=2,
+            fc_in_features_list=[16384, 2048],
+            fc_out_features_list=[2048, 4762],
+        ).to(device=device)
+
+        default_optimizer = optim.Adam(default_model.parameters(), lr=LEARNING_RATE)
+
+        return default_model, default_optimizer
 
 
     @staticmethod
@@ -59,6 +82,7 @@ class Utils:
         for move in game.mainline_moves():
             moves.append(move.uci())
 
+        from backend.api.models import SavedGameContent
         return SavedGameContent(white_elo=white_elo, black_elo=black_elo, result=result, moves=moves)
 
 
