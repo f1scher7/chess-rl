3cc53955b50db811b76840ed7c90f1acb5006249
Maks Szyło <maksym.szylo@gmail.com>
2025-05-21 20:51:14 +0200

    feat(main): switch to loading saved model and optim

--- backend/main.py
+++ backend/main.py
@@ -24,15 +24,15 @@ chess_env = ChessEnv()
 # ).to(device=device)
 
 # FOR 4gb vRAM
-chess_model = ChessPolicy(
-    conv_layers_num=3,
-    in_channels_list=[12, 64, 128],
-    out_channels_list=[64, 128, 256],
-    kernel_size_list=[3, 3, 3],
-    fc_layers_num=2,
-    fc_in_features_list=[16384, 2048],
-    fc_out_features_list=[2048, 4762],
-).to(device=device)
+# chess_model = ChessPolicy(
+#     conv_layers_num=3,
+#     in_channels_list=[12, 64, 128],
+#     out_channels_list=[64, 128, 256],
+#     kernel_size_list=[3, 3, 3],
+#     fc_layers_num=2,
+#     fc_in_features_list=[16384, 2048],
+#     fc_out_features_list=[2048, 4762],
+# ).to(device=device)
 
 # chess_model = ChessPolicy(
 #     conv_layers_num=3,
@@ -44,15 +44,15 @@ chess_model = ChessPolicy(
 #     fc_out_features_list=[8192, 4096, 4762],
 # ).to(device=device)
 
-adam = optim.Adam(chess_model.parameters(), lr=LEARNING_RATE)
 
+model, optimizer = Utils.create_default_model_and_optimizer()
 
 # LOAD MODEL AND OPTIM
-# path = f"{SAVED_MODELS_PATH}model.pth"
-# Utils.load_model(model=chess_model, optimizer=adam, path=path)
+# file_name = f"{SAVED_MODELS_PATH}model.pth"
+# Utils.load_model(model=model, optimizer=optimizer, file_name=file_name)
 
 
 self_play = SelfPlay(device=device)
-self_play.train(env=chess_env, model=chess_model, optimizer=adam)
+self_play.train(env=chess_env, model=model, optimizer=optimizer)
 
 
