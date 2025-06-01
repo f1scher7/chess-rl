cc7a67e50e2ee0fc8a795517ac617fa98b5696f1
Maks Szyło <maksym.szylo@gmail.com>
2025-06-01 20:19:56 +0200

    feat(config): added ACTION_SPACE

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from backend.chess_agent.agent_config import LEARNING_RATE
 from backend.chess_agent.policy import ChessPolicy
 from backend.chess_agent.self_play import SelfPlay
 from backend.chess_env.chess_env import ChessEnv
-from backend.config import SAVED_MODELS_PATH
+from backend.config import SAVED_MODELS_PATH, ACTION_SPACE
 from backend.utils.utils import Utils
 
 
@@ -20,19 +20,19 @@ chess_env = ChessEnv()
 #     kernel_size_list=[3, 3, 3, 3],
 #     fc_layers_num=3,
 #     fc_in_features_list=[65536, 32768, 16384],
-#     fc_out_features_list=[32768, 16384, 4762],
+#     fc_out_features_list=[32768, 16384, ACTION_SPACE],
 # ).to(device=device)
 
 # FOR 4gb vRAM
-# chess_model = ChessPolicy(
-#     conv_layers_num=3,
-#     in_channels_list=[12, 64, 128],
-#     out_channels_list=[64, 128, 256],
-#     kernel_size_list=[3, 3, 3],
-#     fc_layers_num=2,
-#     fc_in_features_list=[16384, 2048],
-#     fc_out_features_list=[2048, 4762],
-# ).to(device=device)
+chess_model = ChessPolicy(
+    conv_layers_num=3,
+    in_channels_list=[12, 64, 128],
+    out_channels_list=[64, 128, 256],
+    kernel_size_list=[3, 3, 3],
+    fc_layers_num=2,
+    fc_in_features_list=[16384, 2048],
+    fc_out_features_list=[2048, ACTION_SPACE],
+).to(device=device)
 
 # chess_model = ChessPolicy(
 #     conv_layers_num=3,
@@ -41,18 +41,18 @@ chess_env = ChessEnv()
 #     kernel_size_list=[3, 3, 3],
 #     fc_layers_num=3,
 #     fc_in_features_list=[16384, 8192, 4096],
-#     fc_out_features_list=[8192, 4096, 4762],
+#     fc_out_features_list=[8192, 4096, ACTION_SPACE],
 # ).to(device=device)
 
 
 model, optimizer = Utils.create_default_model_and_optimizer()
 
 # LOAD MODEL AND OPTIM
-# file_name = f"{SAVED_MODELS_PATH}model.pth"
-# Utils.load_model(model=model, optimizer=optimizer, file_name=file_name)
+# model_file_name = 'chess-rl-model-episodes6000-22-49-34_21-05-2025.pth'
+# Utils.load_model(model=model, optimizer=optimizer, file_name=model_file_name)
 
 
 self_play = SelfPlay(device=device)
-self_play.train(env=chess_env, model=model, optimizer=optimizer)
+self_play.train(env=chess_env, model=model, optimizer=optimizer, init_episode=1)
 
 
