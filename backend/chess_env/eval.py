d6fb2c5d665f9be9c39882e4bdfb3cc0072b0cca
Maks Szyło <maksym.szylo@gmail.com>
2025-06-01 20:18:22 +0200

    refactor(chessEnv): remove @staticmethod adnotations

--- backend/chess_env/eval.py
+++ backend/chess_env/eval.py
@@ -13,32 +13,34 @@ class Eval:
         chess.KING: 0
     }
 
-    white_castling_reward_given = False
-    black_castling_reward_given = False
+
+    def __init__(self, board):
+        self.board = board
+
+        self.white_castling_reward_given = False
+        self.black_castling_reward_given = False
 
 
-    @staticmethod
-    def evaluate_board(board):
+    def evaluate_board(self):
         # weights
         w1, w2, w3, w4, w5, w6 = 2.0, 1.0, 1, 0.75, 0.5, 0.5
 
         evaluation = (
-            w1 * Eval.evaluate_material(board) +
-            w2 * Eval.evaluate_threats(board) +
-            w3 * Eval.evaluate_king_safety(board) +
-            w4 * Eval.evaluate_mobility(board) +
-            w5 * Eval.evaluate_center_control(board) +
-            w6 * Eval.evaluate_pawn_structure(board)
+            w1 * self.evaluate_material() +
+            w2 * self.evaluate_threats() +
+            w3 * self.evaluate_king_safety() +
+            w4 * self.evaluate_mobility() +
+            w5 * self.evaluate_center_control() +
+            w6 * self.evaluate_pawn_structure()
         )
 
         return evaluation
 
 
-    @staticmethod
-    def evaluate_material(board):
+    def evaluate_material(self):
         evaluation = 0
 
-        for square, piece in board.piece_map().items():
+        for square, piece in self.board.piece_map().items():
             value = Eval.piece_values.get(piece.piece_type)
 
             evaluation += value if piece.color == chess.WHITE else -value
@@ -46,28 +48,27 @@ class Eval:
         return evaluation
 
 
-    @staticmethod
-    def evaluate_threats(board):
+    def evaluate_threats(self):
         evaluation = 0
-        legal_moves = set(board.legal_moves)
+        legal_moves = set(self.board.legal_moves)
 
-        for square, piece in board.piece_map().items():
-            sign = 1 if board.piece_at(square).color == chess.WHITE else -1
+        for square, piece in self.board.piece_map().items():
+            sign = 1 if self.board.piece_at(square).color == chess.WHITE else -1
 
             attackers = []
-            for sq in board.attackers(not piece.color, square):
+            for sq in self.board.attackers(not piece.color, square):
                 move = chess.Move(from_square=sq, to_square=square)
                 if move in legal_moves:
-                    board_copy = board.copy()
+                    board_copy = self.board.copy()
                     board_copy.push(move)
                     if not board_copy.is_check():
                         attackers.append(sq)
 
             defenders = []
-            for sq in board.attackers(piece.color, square):
+            for sq in self.board.attackers(piece.color, square):
                 move = chess.Move(from_square=sq, to_square=square)
                 if move in legal_moves:
-                    board_copy = board.copy()
+                    board_copy = self.board.copy()
                     board_copy.push(move)
                     if not board_copy.is_check():
                         defenders.append(sq)
@@ -81,8 +82,8 @@ class Eval:
                 evaluation -= curr_piece_value * sign
                 continue
 
-            attackers_value_sum = Eval.get_pieces_value_sum_by_squares(board=board, squares=attackers)
-            defenders_value_sum = Eval.get_pieces_value_sum_by_squares(board=board, squares=defenders)
+            attackers_value_sum = self.get_pieces_value_sum_by_squares(squares=attackers)
+            defenders_value_sum = self.get_pieces_value_sum_by_squares(squares=defenders)
 
             avg_attackers_value = attackers_value_sum / len(attackers)
 
@@ -95,9 +96,8 @@ class Eval:
         return evaluation
 
 
-    @staticmethod
-    def evaluate_mobility(board):
-        board_copy = board.copy()
+    def evaluate_mobility(self):
+        board_copy = self.board.copy()
 
         board_copy.turn = chess.WHITE
         white_player_moves = len(list(board_copy.legal_moves))
@@ -108,59 +108,56 @@ class Eval:
         return white_player_moves - black_player_moves
 
 
-    @staticmethod
-    def evaluate_king_safety(board):
-        white_king = board.king(chess.WHITE)
-        black_king = board.king(chess.BLACK)
+    def evaluate_king_safety(self):
+        white_king = self.board.king(chess.WHITE)
+        black_king = self.board.king(chess.BLACK)
 
         white_king_safety = sum(
             1 for sq in chess.SquareSet(chess.BB_KING_ATTACKS[white_king])
-            if board.piece_at(sq) is not None and board.piece_at(sq).color == chess.WHITE
+            if self.board.piece_at(sq) is not None and self.board.piece_at(sq).color == chess.WHITE
         ) / 2
 
         black_king_safety = sum(
             1 for sq in chess.SquareSet(chess.BB_KING_ATTACKS[black_king])
-            if board.piece_at(sq) is not None and board.piece_at(sq).color == chess.BLACK
+            if self.board.piece_at(sq) is not None and self.board.piece_at(sq).color == chess.BLACK
         ) / 2
 
-        if len(board.move_stack) >= 2:
-            last_move = board.move_stack[-1]
+        if len(self.board.move_stack) >= 2:
+            last_move = self.board.move_stack[-1]
 
-            if board.piece_at(last_move.to_square).color == chess.WHITE and not Eval.white_castling_reward_given:
-                if board.is_castling(last_move):
+            if self.board.piece_at(last_move.to_square).color == chess.WHITE and not self.white_castling_reward_given:
+                if self.board.is_castling(last_move):
                     white_king_safety += CASTLING_BONUS
-                    Eval.white_castling_reward_given = True
-                elif not board.has_castling_rights(chess.WHITE):
+                    self.white_castling_reward_given = True
+                elif not self.board.has_castling_rights(chess.WHITE):
                     white_king_safety -= CASTLING_BONUS
-                    Eval.white_castling_reward_given = True
+                    self.white_castling_reward_given = True
 
-            elif board.piece_at(last_move.to_square).color == chess.BLACK and not Eval.black_castling_reward_given:
-                if board.is_castling(last_move):
+            elif self.board.piece_at(last_move.to_square).color == chess.BLACK and not self.black_castling_reward_given:
+                if self.board.is_castling(last_move):
                     black_king_safety += CASTLING_BONUS
-                    Eval.black_castling_reward_given = True
-                elif not board.has_castling_rights(chess.BLACK):
+                    self.black_castling_reward_given = True
+                elif not self.board.has_castling_rights(chess.BLACK):
                     black_king_safety -= CASTLING_BONUS
-                    Eval.black_castling_reward_given = True
+                    self.black_castling_reward_given = True
 
         return white_king_safety - black_king_safety
 
 
-    @staticmethod
-    def evaluate_center_control(board):
+    def evaluate_center_control(self):
         center_squares = [chess.D4, chess.D5, chess.E4, chess.E5]
 
         white_control = 0
         black_control = 0
 
         for sq in center_squares:
-            white_control += len(board.attackers(chess.WHITE, sq))
-            black_control += len(board.attackers(chess.BLACK, sq))
+            white_control += len(self.board.attackers(chess.WHITE, sq))
+            black_control += len(self.board.attackers(chess.BLACK, sq))
 
         return white_control - black_control
 
 
-    @staticmethod
-    def evaluate_pawn_structure(board):
+    def evaluate_pawn_structure(self):
         def count_weak_pawns(pawns):
             weak_pawns = 0
 
@@ -190,29 +187,28 @@ class Eval:
 
             return weak_pawns
 
-        white_weak_pawns = count_weak_pawns(board.pieces(chess.PAWN, chess.WHITE))
-        black_weak_pawns = count_weak_pawns(board.pieces(chess.PAWN, chess.BLACK))
+        white_weak_pawns = count_weak_pawns(self.board.pieces(chess.PAWN, chess.WHITE))
+        black_weak_pawns = count_weak_pawns(self.board.pieces(chess.PAWN, chess.BLACK))
 
         return black_weak_pawns - white_weak_pawns
 
 
-    @staticmethod
-    def evaluate_capture_decision(board, move_played):
-        mover_piece = board.piece_at(move_played.from_square)
+    def evaluate_capture_decision(self, move_played):
+        mover_piece = self.board.piece_at(move_played.from_square)
         mover_color = mover_piece.color
 
         reward_or_penalty = 0
         good_captures_list = []
 
-        for move in board.legal_moves:
-            if board.is_capture(move):
-                piece_captured = board.piece_at(move.to_square)
-                piece_moving = board.piece_at(move.from_square)
+        for move in self.board.legal_moves:
+            if self.board.is_capture(move):
+                piece_captured = self.board.piece_at(move.to_square)
+                piece_moving = self.board.piece_at(move.from_square)
 
                 if not piece_captured or not piece_moving or piece_captured.color == mover_color:
                     continue
 
-                board_copy = board.copy()
+                board_copy = self.board.copy()
                 board_copy.push(move)
 
                 if board_copy.is_check():
@@ -234,15 +230,13 @@ class Eval:
         return reward_or_penalty
 
 
-    @staticmethod
-    def get_pieces_value_sum_by_squares(board, squares):
+    def get_pieces_value_sum_by_squares(self, squares):
         return sum(
-            Eval.piece_values.get(board.piece_at(sq).piece_type)
-            for sq in squares if board.piece_at(sq)
+            Eval.piece_values.get(self.board.piece_at(sq).piece_type)
+            for sq in squares if self.board.piece_at(sq)
         )
 
 
-    @staticmethod
-    def reset_castling_rewards():
-        Eval.white_castling_reward_given = False
-        Eval.black_castling_reward_given = False
+    def reset_castling_rewards(self):
+        self.white_castling_reward_given = False
+        self.black_castling_reward_given = False
